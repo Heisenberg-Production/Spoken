03839bd7218abb4fe69a34292e5941b1b8e6c881
joeyvlin <joeyvlin@gmail.com>
2014-10-06 18:39:13 -0700

    camera view working

--- src/com/spokenmessenger/spoken/activities/MainActivity.java
+++ src/com/spokenmessenger/spoken/activities/MainActivity.java
@@ -1,17 +1,28 @@
 package com.spokenmessenger.spoken.activities;
 
+import java.io.File;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.View;
 
+import com.spokenmessenger.spoken.ActionActivity;
 import com.spokenmessenger.spoken.R;
 import com.spokenmessenger.spoken.fragments.ContactsFragment;
 import com.spokenmessenger.spoken.fragments.RecentFragment;
 
 public class MainActivity extends FragmentActivity {
+	
+	public static final String APP_TAG = "Spoken";
+	private static final int ACTION_VIEW_REQUEST_CODE = 123;
+	public static final String TEMP_PHOTO_FILE = "spoken-temp.jpg";
 	
 	RecentFragment recentFragment;
 	ContactsFragment contactsFragment;
@@ -20,6 +31,16 @@ public class MainActivity extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		if(cameraNotDetected()){
+        	Log.d("debug", "No camera detected, clicking the button below will have unexpected behaviour.");
+        } else {
+        	Log.d("debug", "Camera detected");
+        }
+	}
+	
+	private boolean cameraNotDetected() {
+		return !getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
 	}
 	
 	// Show Recent Messages Fragment
@@ -49,6 +70,36 @@ public class MainActivity extends FragmentActivity {
 	
 	// Show Camera View
 	public void onTapRedCircle(View v) {
-		
+//		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//	    intent.putExtra(MediaStore.EXTRA_OUTPUT, getPhotoFileUri(TEMP_PHOTO_FILE)); 
+		Intent intent = new Intent(this, ActionActivity.class);
+	    startActivityForResult(intent, ACTION_VIEW_REQUEST_CODE);
+	}
+	
+	// Returns the Uri for a photo stored on disk given the fileName
+	public Uri getPhotoFileUri(String fileName) {
+	    // Get safe storage directory for photos
+	    File mediaStorageDir = new File(
+	        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), APP_TAG);
+
+	    // Create the storage directory if it does not exist
+	    if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+	        Log.d(APP_TAG, "failed to create directory");
+	    }
+
+	    // Return the file target for the photo based on filename
+	    return Uri.fromFile(new File(mediaStorageDir.getPath() + File.separator + fileName));
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//		if(requestCode == REQ_CAMERA_IMAGE && resultCode == RESULT_OK){
+//			String imgPath = data.getStringExtra(CameraActivity.EXTRA_IMAGE_PATH);
+//			Log.i("Got image path: "+ imgPath);
+//			displayImage(imgPath);
+//		} else
+//		if(requestCode == REQ_CAMERA_IMAGE && resultCode == RESULT_CANCELED){
+//			Log.i("User didn't take an image");
+//		}
 	}
 }
